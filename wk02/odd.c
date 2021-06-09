14050a4c77b768b1d12e0bd2c7211ee6a3c23ac9
Selina Chua <selina.a.chua@gmail.com>
2021-06-09 12:34:20 +1000

    Added files from today.

---
+++ wk02/odd.c
@@ -0,0 +1,38 @@
+/**
+ * Program that checks if a number is odd using bitwise operations.
+ */
+
+#include <stdio.h>
+
+int is_odd(int num);
+
+int main(void) {
+    printf("Please enter a number: ");
+    int num = 0;
+    scanf("%d", &num);
+
+    if (is_odd(num)) {
+        printf("ODD\n");
+    } else {
+        printf("EVEN\n");
+    }
+
+    return 0;
+}
+
+/**
+ * Function that checks if an int is odd or even using bit masking.
+ *
+ * 0111 1010 -> even
+ * 0111 1011 -> odd
+ */
+int is_odd(int num) {
+    int mask = 1; // 0000 0001 
+    int result = num & mask;
+
+    if (result == 1) {
+        return 1;
+    }
+
+    return 0;
+}
